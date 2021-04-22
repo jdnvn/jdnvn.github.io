3c6694b0c9c3b6567b5f55efa11d14b9bdef5b06
Joey Donovan <jdnvn>
2021-04-22 15:36:30 -0400

    delete files

--- src/context/context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import projectsData from '../mock/data';
-
-const PortfolioContext = React.createContext(projectsData);
-
-export const PortfolioProvider = PortfolioContext.Provider;
-export const PortfolioConsumer = PortfolioContext.Consumer;
-
-export default PortfolioContext;
